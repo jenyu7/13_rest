1c509a05d7c506232a40487f20d74502d9e9cbe2
jenyu7 <jyu7@stuy.edu>
2017-11-09 23:26:45 -0500

    i should be studying for gov

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def welcome():
     data = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=hTUDYhfxSNhyewvcLn1JJX1hl7tnbIpvNrBmgPqi")
     d = json.loads(data.read())
     print d;
-    return render_template('index.html',title = d["title"], pic0url = d["url"], exp = d["explanation"])
+    return render_template('index.html',title = d["title"], pic0url = d["url"], exp = d["explanation"], c = d["copyright"], d = d["date"])
 
 if __name__ == "__main__":
     app.debug = True
