8ef89f78e41ae6913cf78ce01f9db3ec0e12cc2c
jenyu7 <jyu7@stuy.edu>
2017-11-09 22:38:33 -0500

    y does my css not work

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+'''
+Jen Yu
+Period 7 SoftDev
+K #13: A RESTful Journey Skyward
+2017-11-09
+'''
 from flask import Flask, render_template
 import urllib2, json
 app = Flask(__name__)
@@ -7,7 +13,7 @@ def welcome():
     data = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=hTUDYhfxSNhyewvcLn1JJX1hl7tnbIpvNrBmgPqi")
     d = json.loads(data.read())
     print d;
-    return render_template('index.html', picurl = d["url"])
+    return render_template('index.html',title = d["title"], pic0url = d["url"], exp = d["explanation"])
 
 if __name__ == "__main__":
     app.debug = True
